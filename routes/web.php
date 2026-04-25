7bc355f9a72f42f0c169ca41556c7dab5ca9fd99
thestrelchik <thestrelchik@users.noreply.github.com>
2026-04-25 14:15:14 +0300

    feat: prepared Admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/admin', [MainController::class, 'index'])-> name('admin.name.index');
+Route::get('/admin', [MainController::class, 'index'])-> name('admin.main.index');
