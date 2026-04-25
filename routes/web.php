41cbfa2633e8414ff83900b0e59441d81d0b8af9
thestrelchik <thestrelchik@users.noreply.github.com>
2026-04-25 12:47:42 +0300

    feat: admin page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', [MainController::class, 'index'])-> name('admin.name.index');
